35dd0da72ff8b09faabecde4b81ce3611e570a77
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2025-09-28 13:25:28 +0600

    Email issue fixed

--- UniLib/settings.py
+++ UniLib/settings.py
@@ -167,7 +167,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
 
 # Email settings
-DEFAULT_FROM_EMAIL = 'UniLib Support <noreply@unilib.com>'
+DEFAULT_FROM_EMAIL = 'UniLib <talukdermahedihasan@gmail.com>'
 SERVER_EMAIL = 'server@unilib.com'  # For error messages
 
 # Sites framework
