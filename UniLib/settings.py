7b4fdc9d0fa6e3e8ed3b42b1cda2ecf053c5f353
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2025-09-28 14:15:04 +0600

    try to solve server timeout problem again

--- UniLib/settings.py
+++ UniLib/settings.py
@@ -158,9 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# For development/testing on Render
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
