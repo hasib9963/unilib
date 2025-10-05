424f29af8871eaf5e2af2e7096e8aa2d0a6d8c81
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2025-10-05 21:22:38 +0600

    Internal Server Error try to resolve

--- UniLib/settings.py
+++ UniLib/settings.py
@@ -167,8 +167,8 @@ EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
 
 # Email settings
-DEFAULT_FROM_EMAIL = 'UniLib <noreply@unilib.com>'
-SERVER_EMAIL = 'server@unilib.com'  # For error messages
+DEFAULT_FROM_EMAIL = env('EMAIL')
+SERVER_EMAIL = env('EMAIL')  # For error messages
 
 # Sites framework
 SITE_ID = 1
